4e8e48108de964e773f761e91feaff15dd4d7e53
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-05 00:21:03 +0600

    deconstruct app parts into smaller specific components

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,18 @@
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-  timestamp: number; // time | Date
-}
-
-const DUMMY_DATA: Todo[] = [
-  {
-    id: 1,
-    text: 'Learn React',
-    completed: false,
-    timestamp: Date.now(),
-  },
-  {
-    id: 2,
-    text: 'Learn Redux',
-    completed: false,
-    timestamp: Date.now(),
-  },
-  {
-    id: 3,
-    text: 'Learn TypeScript',
-    completed: false,
-    timestamp: Date.now(),
-  },
-  {
-    id: 4,
-    text: 'Learn React Native',
-    completed: false,
-    timestamp: Date.now(),
-  },
-  {
-    id: 5,
-    text: 'Learn GraphQL',
-    completed: false,
-    timestamp: Date.now(),
-  },
-];
+import CreateTodo from './components/CreateTodo';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div>
       <h1>React Todo App</h1>
       {/* Section 1 - Create Todo*/}
-      <div>
-        <label>
-          New Todo:
-          <input type="text" />
-        </label>
-        <button>Add New Todo</button>
-      </div>
+      <CreateTodo />
       {/* Section 2 - Todo List Display */}
       {/* add filters probably */}
       <div>
         <h2>Todo List</h2>
         {/* Section 3 - Todo Item */}
-        {DUMMY_DATA.map((todo) => (
-          <div key={todo.id}>
-            <h3>{todo.text}</h3>
-            <p>{new Date(todo.timestamp).toLocaleTimeString()}</p>
-            <p>{new Date(todo.timestamp).toLocaleDateString()}</p>
-            {/* Section 4 - User Interaction */}
-            <button>Complete</button>
-            <button>Delete</button>
-          </div>
-        ))}
+        <TodoList />
       </div>
     </div>
   );
