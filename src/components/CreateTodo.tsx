b2f8b5c06cc9b457914df08d6387f2192cbfb59c
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-05 00:34:19 +0600

    add todo fn

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 export default function CreateTodo() {
+  const [todo, setTodo] = useState('');
+
+  const createTodo = () => {
+    if (todo) {
+      console.log('todo', todo);
+    }
+  };
+
   return (
     <div>
       <label>
         New Todo:
-        <input type="text" />
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
       </label>
-      <button>Add New Todo</button>
+      <button onClick={createTodo}>Add New Todo</button>
     </div>
   );
 }
