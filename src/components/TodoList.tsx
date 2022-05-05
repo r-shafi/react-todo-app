05d72e46d6aa58e2b2f236080dd5809bd4ecf1a2
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-05 23:16:54 +0600

    todo list layout style

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,6 +5,8 @@ export default function TodoList() {
   const { state } = useLocalStorageContext();
 
   return (
-    <>{state && state.map((todo) => <TodoItem key={todo.id} todo={todo} />)}</>
+    <div className="flex flex-col gap-4">
+      {state && state.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
+    </div>
   );
 }
