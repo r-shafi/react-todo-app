73395f4d5cb4bd6798c9e9afdb72ed1cf319a04e
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-05 23:39:08 +0600

    app title style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="px-4 md:max-w-screen-sm mx-auto">
       <LocalStorageContextProvider>
-        <h1>React Todo App</h1>
+        <h1 className="font-bold uppercase my-2">React Todo App</h1>
         <CreateTodo />
         <div>
           <TodoList />
