5c20872f055b2a838a0848d519b6bcf678956d7f
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-06 23:41:46 +0600

    extract reducer function

--- src/contexts/LocalStorageContext.tsx
+++ src/contexts/LocalStorageContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import { Todo } from '../models/Todo';
+import { TodoStateReducer } from '../utilities/TodoStateReducer';
 
 export const LocalStorageContext = createContext<any>(null);
 
@@ -7,71 +8,14 @@ interface Props {
   children: React.ReactNode;
 }
 
-let NEW_STATE: Todo[] = [];
-let TEMP_STATE: Todo[] = [];
-
-function reducer(state: Todo[] | [], action: any) {
-  let temp = false;
-
-  switch (action.type) {
-    case 'ADD':
-      NEW_STATE = [...state, action.payload];
-      break;
-    case 'REMOVE':
-      NEW_STATE = state.filter((item) => item.id !== action.payload);
-      break;
-    case 'TOGGLE_COMPLETE':
-      NEW_STATE = state.map((item) => {
-        if (item.id === action.payload) {
-          return { ...item, completed: !item.completed };
-        }
-        return item;
-      });
-      break;
-    case 'SEARCH_TODO':
-      temp = true;
-
-      if (!NEW_STATE.length) {
-        NEW_STATE = JSON.parse(localStorage.getItem('todos')!) || [];
-      }
-
-      if (!action.payload) {
-        TEMP_STATE = NEW_STATE;
-      } else {
-        TEMP_STATE = NEW_STATE.filter((item) =>
-          item.title.toLowerCase().includes(action.payload.toLowerCase())
-        );
-      }
-      break;
-    case 'REFRESH':
-      temp = false;
-      if (!NEW_STATE.length) {
-        NEW_STATE = JSON.parse(localStorage.getItem('todos')!) || [];
-      }
-      break;
-    case 'COMPLETED_TODOS':
-      temp = true;
-      TEMP_STATE = NEW_STATE.filter((item) => item.completed);
-      break;
-    default:
-      return state;
-  }
-
-  if (!temp) {
-    localStorage.setItem('todos', JSON.stringify(NEW_STATE));
-  }
-
-  return temp ? TEMP_STATE : NEW_STATE;
-}
-
 export const LocalStorageContextProvider = ({ children }: Props) => {
   const localData: Todo[] = JSON.parse(localStorage.getItem('todos')!) || [];
 
-  const [state, dispatch] = useReducer(reducer, localData);
+  const [state, dispatch] = useReducer(TodoStateReducer, localData);
 
   return (
     <LocalStorageContext.Provider value={{ state, dispatch }}>
-      <div>{children}</div>
+      {children}
     </LocalStorageContext.Provider>
   );
 };
